af78923c5ddeb52bd3fbda9a0bd46dedf2d88c25
Luiz Fernando <luizfernandosant26@gmail.com>
2026-01-07 11:32:27 -0300

    project recreated

--- backend/src/main/java/com/capisite/backend/BackendApplication.java
+++ backend/src/main/java/com/capisite/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.capisite.backendweddingsite;
+package com.capisite.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendweddingsiteApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendweddingsiteApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
