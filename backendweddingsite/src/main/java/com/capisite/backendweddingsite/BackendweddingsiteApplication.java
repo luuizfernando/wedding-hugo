fc1f5722f23b2d2559c87107551720aea5d7bb08
Luiz Fernando <luizfernandosant26@gmail.com>
2026-01-07 11:30:35 -0300

    backend project created

---
+++ backendweddingsite/src/main/java/com/capisite/backendweddingsite/BackendweddingsiteApplication.java
@@ -0,0 +1,13 @@
+package com.capisite.backendweddingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendweddingsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendweddingsiteApplication.class, args);
+	}
+
+}
---
+++ backendweddingsite/src/test/java/com/capisite/backendweddingsite/BackendweddingsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capisite.backendweddingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendweddingsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
